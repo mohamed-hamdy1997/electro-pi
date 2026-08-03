e414121ff5ad8b8553b560d6877549e1768a11fa
Mohamed Hamdy <mohamedhamdy.o1997@gmail.com>
2026-08-03 07:40:19 +0300

    fix: add Swagger UI assets to public for Railway deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(
+            at: '*',
+            headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO,
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
